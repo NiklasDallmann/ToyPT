f67265e589a5e8f2a16eb134bd96a7d3f864aa08
Niklas Dallmann <git@niklas-dallmann.de>
2019-06-26 20:03:10 +0200

    Move algorithms to header

--- include/math/algorithms.h
+++ include/math/algorithms.h
@@ -1,6 +1,8 @@
 #ifndef ALGORITHMS_H
 #define ALGORITHMS_H
 
+#include <algorithm>
+
 namespace Math
 {
 
@@ -9,8 +11,16 @@ constexpr float epsilon = 1e-6;
 template <typename T>
 bool fuzzyCompareEqual(const T left, const T right);
 
-template <>
-bool fuzzyCompareEqual<float>(const float, const float);
+template<>
+bool fuzzyCompareEqual<float>(const float left, const float right)
+{
+	bool returnValue = false;
+	
+	returnValue = std::abs(std::abs(left) - std::abs(right)) < epsilon;
+	
+	return returnValue;
+}
+
 
 } // namespace Math
 
--- src/math/algorithms.cpp
+++ src/math/algorithms.cpp
@@ -1,18 +1,1 @@
-#include <algorithm>
-
 #include "algorithms.h"
-
-namespace Math
-{
-
-template<>
-bool fuzzyCompareEqual<float>(const float left, const float right)
-{
-	bool returnValue = false;
-	
-	returnValue = std::abs(std::abs(left) - std::abs(right)) < epsilon;
-	
-	return returnValue;
-}
-
-}
